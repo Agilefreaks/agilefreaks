533d9e5e08b84502be522b688ae4624b205e9e08
illy <ileana.tristiu@agilefreaks.com>
2012-10-22 12:46:15 +0300

    initial version of the site

---
+++ AgileFreaks/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using AgileFreaks.Models;
+
+namespace AgileFreaks.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var todoItem = new TodoItem { Title = "Single Page Applicatio Test" };
+
+            return View(todoItem);
+        }
+    }
+}
