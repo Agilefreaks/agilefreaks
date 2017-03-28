5b080542035e5d0ce5606eb8beb3db56c85bedc7
Mihai <mihaiiftode@users.noreply.github.com>
2017-03-29 00:33:22 +0300

    [#23] Framework update and bug fixes related to it
    [20] Changes features (#18)
    
    [#23] Updates nuggets to the latest version, removes sendgrid
    
    [#23] Bump framework to 4.6.2
    
    [#23] Remove sendgrid dependency completely
    
    [#23] Adds yarn support
    
    [#23] Adds Slick, replaces old boxSlider which is not support on latest jQuery'
    
    [#23] Add Slick bundle items
    
    [#23] Update style and html for slick usage
    
    [#23] Add support for slick a fix small bug
    
    [#23] Add yarn waypoints and change implementation
    
    [#23] Add waypoints manually
    
    [#23] Remove yarn
    
    Remove .vs folder from git

--- Agilefreaks/Controllers/ContactUsController.cs
+++ Agilefreaks/Controllers/ContactUsController.cs
@@ -4,13 +4,11 @@ using System.Net;
 using System.Net.Mail;
 using System.Web.Mvc;
 using Agilefreaks.Models;
+using System.Threading.Tasks;
+
 
 namespace Agilefreaks.Controllers
 {
-    using System.Threading.Tasks;
-
-    using SendGrid;
-
     public class ContactUsController : Controller
     {
         private const string ContactEmail = "office@agilefreaks.com";
@@ -22,30 +20,6 @@ namespace Agilefreaks.Controllers
             {
                 return Redirect(Url.RouteUrl(new { controller = "Home", action = "Index" }) + "#" + "contact");
             }
-
-            var message = new SendGridMessage
-                              {
-                                  From = new MailAddress(model.Email),
-                                  Subject = model.Name,
-                                  Text = model.Message
-                              };
-            message.AddTo(ContactEmail);
-            var notificationMessage = "Message sent.";
-
-            try
-            {
-                var credentials = new NetworkCredential(
-                    ConfigurationManager.AppSettings["SendGridUsername"],
-                    ConfigurationManager.AppSettings["SendGridPassword"]);
-               var transportWeb = new Web(credentials);
-               await transportWeb.DeliverAsync(message);
-            }
-            catch (Exception)
-            {
-                notificationMessage = "Error on sending message. Please try again!";
-            }
-
-            TempData["Notification"] = notificationMessage;
 
             return Redirect(Url.RouteUrl(new { controller = "Home", action = "Index" }) + "#" + "contact");
         }
