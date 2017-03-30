8531629d21b2e081bec575a5b1f041273d96a47c
Mihai Iftode <mihai.iftode94@gmail.com>
2017-03-30 12:25:42 +0300

    [#15] Add postmark and unit tests

---
+++ Agilefreaks.Tests/ContactUsControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using Agilefreaks.Controllers;
+using Agilefreaks.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PostmarkDotNet;
+
+namespace Agilefreaks.Tests
+{
+    [TestClass]
+    public class ContactUsControllerTest
+    {
+        private PostmarkMessage _postmarkMessage;
+        private ContactUsModel _contactUsModel;
+        private ContactUsController _contactUsController;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _contactUsController = new ContactUsController();
+            _postmarkMessage = new PostmarkMessage
+            {
+                To = "office@agilefreaks.com",
+                From = "test@test.com",
+                TrackOpens = true,
+                Subject = "A jobless man",
+                TextBody = "Hire me!"
+            };
+        }
+
+        [TestMethod]
+        public void TestCreateMessage()
+        {
+            _contactUsModel = new ContactUsModel
+            {
+                Email = "test@test.com",
+                Message = "Hire me!",
+                Name = "A jobless man"
+            };
+
+            var result = _contactUsController.CreateMessage(_contactUsModel);
+
+            Assert.AreSame(_postmarkMessage.To, result.To);
+            Assert.AreSame(_postmarkMessage.From, result.From);
+            Assert.AreSame(_postmarkMessage.Subject, result.Subject);
+            Assert.AreSame(_postmarkMessage.TextBody, result.TextBody);
+        }
+
+        [TestMethod]
+        public async Task TestSendMailMessage()
+        {
+            var result = await _contactUsController.SendMailMessage("POSTMARK_API_TEST", _postmarkMessage);
+
+            Assert.AreEqual(PostmarkStatus.Success, result);
+        }
+    }
+}
--- Agilefreaks/Controllers/ContactUsController.cs
+++ Agilefreaks/Controllers/ContactUsController.cs
@@ -5,6 +5,9 @@ using System.Net.Mail;
 using System.Web.Mvc;
 using Agilefreaks.Models;
 using System.Threading.Tasks;
+using System.Web.Management;
+using PostmarkDotNet;
+using PostmarkDotNet.Model;
 
 
 namespace Agilefreaks.Controllers
@@ -18,10 +21,45 @@ namespace Agilefreaks.Controllers
         {
             if (!ModelState.IsValid)
             {
-                return Redirect(Url.RouteUrl(new { controller = "Home", action = "Index" }) + "#" + "contact");
+                return Redirect(Url.RouteUrl(new {controller = "Home", action = "Index"}) + "#" + "contact");
+            }
+            var message = CreateMessage(model);
+            var apiKey = Environment.GetEnvironmentVariable("POSTMARK_APIKEY");
+
+            await SendMailMessage(apiKey, message);
+
+            return Redirect(Url.RouteUrl(new {controller = "Home", action = "Index"}) + "#" + "contact");
+        }
+
+        public PostmarkMessage CreateMessage(ContactUsModel model)
+        {
+            return new PostmarkMessage
+            {
+                To = ContactEmail,
+                From = model.Email,
+                Subject = model.Name,
+                TextBody = model.Message
+            };
+        }
+        
+        public async Task<PostmarkStatus> SendMailMessage(string apiKey, PostmarkMessage message)
+        {
+            const string errorMessage = "Error on sending message. Please try again!";
+            var sendStatus = PostmarkStatus.ServerError;
+
+            try
+            {
+                var client = new PostmarkClient(apiKey);
+                var sendResult = await client.SendMessageAsync(message);
+                TempData["Notification"] = sendResult.Status == PostmarkStatus.Success ? "Message sent." : errorMessage;
+                sendStatus = sendResult.Status;
+            }
+            catch(Exception e)
+            {
+                TempData["Notification"] = errorMessage;
             }
 
-            return Redirect(Url.RouteUrl(new { controller = "Home", action = "Index" }) + "#" + "contact");
+            return sendStatus;
         }
     }
 }
