bd6b7df3152427f73c8406dbe4469103e1a95955
Mihai <mihaiiftode@users.noreply.github.com>
2017-03-30 15:18:01 +0300

    [#15] Add postmark support for sending mails (#20)
    * [#15] Add postmark and unit tests
    
    * [#15] Remove exception
    
    * [#15] Remove unused references
    
    * [#15] Change implementation for sending details

---
+++ Agilefreaks.Tests/ContactUsControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Agilefreaks.Controllers;
+using Agilefreaks.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PostmarkDotNet;
+
+namespace Agilefreaks.Tests
+{
+    [TestClass]
+    public class ContactUsControllerTest
+    {
+        private PostmarkMessage _postmarkMessage;
+        private ContactUsModel _contactUsModel;
+        private ContactUsController _contactUsController;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _contactUsController = new ContactUsController();
+            _postmarkMessage = new PostmarkMessage
+            {
+                To = "office@agilefreaks.com",
+                From = "office@agilefreaks.com",
+                Subject = "A jobless man",
+                TextBody = "Hire me!",
+                ReplyTo = "test@test.com"
+            };
+        }
+
+        [TestMethod]
+        public void TestCreateMessage()
+        {
+            _contactUsModel = new ContactUsModel
+            {
+                Email = "test@test.com",
+                Message = "Hire me!",
+                Name = "A jobless man"
+            };
+
+            var result = _contactUsController.CreateMessage(_contactUsModel);
+
+            Assert.AreSame(_postmarkMessage.To, result.To);
+            Assert.AreSame(_postmarkMessage.From, result.From);
+            Assert.AreSame(_postmarkMessage.Subject, result.Subject);
+            Assert.AreSame(_postmarkMessage.TextBody, result.TextBody);
+        }
+
+        [TestMethod]
+        public async Task TestSendMailMessage()
+        {
+            var result = await _contactUsController.SendMailMessage("POSTMARK_API_TEST", _postmarkMessage);
+
+            Assert.AreEqual(PostmarkStatus.Success, result);
+        }
+    }
+}
--- Agilefreaks/Controllers/ContactUsController.cs
+++ Agilefreaks/Controllers/ContactUsController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Configuration;
-using System.Net;
-using System.Net.Mail;
 using System.Web.Mvc;
 using Agilefreaks.Models;
 using System.Threading.Tasks;
+using PostmarkDotNet;
 
 
 namespace Agilefreaks.Controllers
@@ -12,16 +10,53 @@ namespace Agilefreaks.Controllers
     public class ContactUsController : Controller
     {
         private const string ContactEmail = "office@agilefreaks.com";
+        private const string SenderEmail = "office@agilefreaks.com";
 
         [HttpPost]
         public async Task<ActionResult> SendEmail(ContactUsModel model)
         {
             if (!ModelState.IsValid)
             {
-                return Redirect(Url.RouteUrl(new { controller = "Home", action = "Index" }) + "#" + "contact");
+                return Redirect(Url.RouteUrl(new {controller = "Home", action = "Index"}) + "#" + "contact");
+            }
+            var message = CreateMessage(model);
+            var apiKey = Environment.GetEnvironmentVariable("POSTMARK_APIKEY");
+
+            await SendMailMessage(apiKey, message);
+
+            return Redirect(Url.RouteUrl(new {controller = "Home", action = "Index"}) + "#" + "contact");
+        }
+
+        public PostmarkMessage CreateMessage(ContactUsModel model)
+        {
+            return new PostmarkMessage
+            {
+                To = ContactEmail,
+                From = SenderEmail,
+                Subject = model.Name,
+                TextBody = model.Message,
+                ReplyTo = model.Email
+            };
+        }
+        
+        public async Task<PostmarkStatus> SendMailMessage(string apiKey, PostmarkMessage message)
+        {
+            const string errorMessage = "Error on sending message. Please try again!";
+            var sendStatus = PostmarkStatus.ServerError;
+
+            try
+            {
+                var client = new PostmarkClient(apiKey);
+                var sendResult = await client.SendMessageAsync(message);
+                TempData["Notification"] = sendResult.Status == PostmarkStatus.Success ? "Message sent." : errorMessage;
+                sendStatus = sendResult.Status;
+            }
+            catch
+            {
+                TempData["Notification"] = errorMessage;
             }
 
-            return Redirect(Url.RouteUrl(new { controller = "Home", action = "Index" }) + "#" + "contact");
+            return sendStatus;
         }
     }
 }
