65fba2d16bf61efd5467c956bcce55be55bf2131
pirvudoru <pirvu.doru@ymail.com>
2018-12-31 00:21:02 +0200

    Update indentation for js files

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
-hbs.registerHelper('currentYear', function() { return new Date().getFullYear() });
+hbs.registerHelper('currentYear', function () {
+  return new Date().getFullYear()
+});
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,12 +28,12 @@ app.use(indexRouter);
 app.use(contactRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404, 'Ooops!'));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/contact.js
+++ routes/contact.js
@@ -5,25 +5,25 @@ const toEmail = "office@agilefreaks.com";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-router.post('/contact', function(req, res, next) {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-    const msg = {
-        to: toEmail,
-        from: email,
-        replyTo: email,
-        subject: "[AgileFreaks.com] " + name,
-        text: message
-    };
+router.post('/contact', function (req, res, next) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const msg = {
+    to: toEmail,
+    from: email,
+    replyTo: email,
+    subject: "[AgileFreaks.com] " + name,
+    text: message
+  };
 
-    sgMail.send(msg)
-        .then(function () {
-            res.render('index', { successMessage: 'Message sent.' });
-        })
-        .catch(function (e) {
-            res.render('index', { errorMessage: 'There has been an error processing your request. Please try sending the email manually to ' + toEmail });
-        });
+  sgMail.send(msg)
+    .then(function () {
+      res.render('index', {successMessage: 'Message sent.'});
+    })
+    .catch(function (e) {
+      res.render('index', {errorMessage: 'There has been an error processing your request. Please try sending the email manually to ' + toEmail});
+    });
 
 });
 
